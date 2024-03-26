aba572349cf4249b82eead2946b2cf1f4a9d1a4f
Lilianademo <123247173+Lilianademo@users.noreply.github.com>
2024-03-26 15:29:04 +0200

    07-phonebook

---
+++ src/redux/operations.js
@@ -0,0 +1,39 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://6602ca3f9d7276a755543ddd.mockapi.io/"
+export const getAllContacts = createAsyncThunk(
+  "contacts/fetchAll",
+  async (thunkAPI) => {
+    try {
+      const response = await axios.get("/contacts");
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const createContact = createAsyncThunk(
+  "contacts/addContact",
+  async (contact, thunkAPI) => {
+    try {
+      const response = await axios.post("/contacts", contact);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  "contacts/deleteContact",
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${id}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
